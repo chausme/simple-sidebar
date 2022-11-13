ee1a1b9a5a7a612ffb79302b241f38313bf8b5bf
Alex Chaus <chausme@gmail.com>
2022-11-13 18:20:44 +0700

    Convert to grid layout and add logic

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+const sidebarToggle = document.querySelector('.sidebar-toggle');
+const sidebar = document.querySelector('.sidebar');
+const closeButton = document.querySelector('.close-btn');
 
+sidebarToggle.addEventListener('click', e => {
+    sidebarToggle.classList.toggle('collapsed');
+    sidebar.classList.toggle('collapsed');
+});
+
+closeButton.addEventListener('click', () => {
+    sidebarToggle.classList.remove('collapsed');
+    sidebar.classList.remove('collapsed');
+});
